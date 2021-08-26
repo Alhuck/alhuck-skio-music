24cfa2e9f43dbe4b1861d1dc4fbc146051a326c5
Alhuck <alhucka@gmail.com>
2021-08-26 09:25:07 -0400

    added typescript support to the project and renamed the files accordingly

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+
+  // const logo = require("./logo.svg") as string;
+
+  return (
+    <div className="App">
+      <header>
+        This is header
+      </header>
+      <main>
+        <p>Main section!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
